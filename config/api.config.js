2f6429215fbe63e4793563a363753ab57086c02e
Gupern <guperner@hotmail.com>
2021-05-22 23:11:05 +0800

    feat: 优化打包配置

--- config/api.config.js
+++ config/api.config.js
@@ -4,5 +4,5 @@ const isPro = Object.is(process.env.NODE_ENV, 'production')
 console.log(isPro);
 
 module.exports = {
-  baseUrl: isPro ? 'https://www.***/index.php/Official（线上地址）' : 'api/'
+  baseUrl: isPro ? 'http://blog.gupern.com/api/' : 'api/'
 }
